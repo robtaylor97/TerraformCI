80fd070fde965080f48d8fc8fcf8855b885ffd39
Rob Taylor <rob-taylor@pluralsight.com>
2023-03-06 05:28:39 +0000

    add infra

---
+++ main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_version = "~> 1.3.0"
+  required_providers {
+    azurerm = {
+      "source" = "hashicorp/azurerm"
+      version  = "3.43.0"
+    }
+  }
+  cloud {
+    organization = "Rob-Taylor"
+    workspaces {
+      name = "TerraformCI"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+}
+
+resource "random_string" "uniquestring" {
+  length  = 20
+  special = false
+  upper   = false
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "811-b670457a-provide-continuous-delivery-with-gith"
+  location = "centralus"
+}
+
+resource "azurerm_storage_account" "storageaccount" {
+  name                     = "stg${random_string.uniquestring.result}"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
